4f530a4bccd0e3f0574c99efd8979fb8c534921d
allhorrorshow <maximalyshev01@gmail.com>
2023-11-03 02:54:47 +0300

    Update robot.ino

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,4 +1,5 @@
 #include <IRremote.h> // библиотека для ИК датчика
+#include <DHT.h>  // библиотека для температуры
 #define PIN_A1 18
 #define PIN_A2 19
 #define PIN_B1 16
@@ -6,10 +7,11 @@
 #define echoPin 25 // Echo Pin
 #define trigPin 26 // Trigger Pin
 IRrecv irrecv(12); // ИК датчик
+DHT dht(27, DHT11);  // сообщаем на каком порту будет датчик
 
 
 
-decode_results results; // хз
+decode_results results; // ИК датчик 
 const int MaxDistanceMeter = 10; // максимально допустимое число для дальномера в СМ (сколько до предмета)
 int MinimumRange = 0; // Минимальное допустимое значение в СМ
 long duration, distance; // Длительность, используемая для вычисления расстояния, растояние 
@@ -27,6 +29,7 @@ void setup() {
   pinMode(PIN_B2, OUTPUT);
   irrecv.enableIRIn(); // вкл ИК приемник 
   Serial.begin(115200);
+  dht.begin();         // запускаем датчик DHT11
 }
 
 
@@ -36,37 +39,51 @@ void loop()
     last_time = millis();
     calculate_distance();
   }
-  if ( irrecv.decode( &results )) {  // возвращает код кнопки с пульта 
+
+  if (irrecv.decode(&results)) {  // возвращает код кнопки с пульта 
     Serial.println(results.value, HEX);
 
-    if (results.value==0xFF02FD && speed < 255){ // тут надо кнопку + прописать, увеличение скорости при нажатии кнопки
-      speed += 30;
+    if (results.value==0x490 && speed < 255 && speed+30 <= 255){ // увеличение скорости при нажатии кнопки
+        speed+=30;
+      Serial.print(speed);
     }
-    if (results.value==0xFF02FD && speed > 0){ // тут надо кнопку - прописать, уменьшение скорости при нажатии кнопки
-      speed -= 30;
+    if (results.value==0xC90 && speed > 0 && speed-30 >= 50){ // уменьшение скорости при нажатии кнопки
+        speed-=30;
+      Serial.print(speed);
     }
 
-    if (results.value==0xFF629D){ // вперед на пульте
+    if (results.value==0x9CB47){ // вперед на пульте
       forward();
-      Serial.print("jopa");
+      
     }
-    if (results.value==0xFFA857){ // назад
+    if (results.value==0x5CB47){ // назад
       backward();
     }   
 
-    if (results.value==0xFF22DD){ // влево
+    if (results.value==0xDCB47){ // влево
       left();
     }
-    if (results.value==0xFFC23D){ // вправо
+    if (results.value==0x3CB47){ // вправо
       right();
     }
-    if (results.value==0xFF02FD){ // стоп
+    if (results.value==0xBCB47){ // стоп
       stop();
     }
     
     irrecv.resume();  
   }
+  // считываем температуру (t) и влажность (h)
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+
+   // выводим температуру (t) и влажность (h) на монитор порта
+   Serial.print("Humidity: ");
+   Serial.println(h);
+   Serial.print("Temperature: ");
+   Serial.println(t);
 }
+
+
 
 
 void forward() // Движение вперед
@@ -121,12 +138,13 @@ int calculate_distance()      //Следующая функция возвращ
  duration = pulseIn(echoPin, HIGH);
  distance = duration/58.2;  //расстояние (в см) исходя из скорости звука.
 if(distance >= MaxDistanceMeter || distance <= MinimumRange){
-  Serial.println(distance); // далеко 
+  //Serial.println(distance); // далеко 
 }
 else{
-  Serial.println(distance); // близко
+  //Serial.println(distance); // близко
   stop();
-  Serial.print("jopa");
 }
 return distance;
-}
+} 
+
+
